38ed7d419aaf956a29b58c9e59b5500d89e14b33
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-05-04 10:50:26 +0100

    added logo to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,28 @@
+import { Link } from "react-router-dom"
+import  Logo  from '../../assets/logo.svg'
 
 function Navbar() {
 
     return (
     <div className="row">
         <div className="container-xxl position-relative p-0">
-            <nav className="nav navbar-expand-lg navbar-dark
+            <nav className="nav navbar-expand-lg bg-info
             px-4 px-lg-5 py-3 py-lg-0 sticky-top">
 
+                {/* Brand Logo */}
+                <Link to='/' className='navbar-brand p-3'>
+                    <img src={ Logo } alt="Brand-Logo"
+                    className='px-lg-4 px-sm-2' />
+                </Link>
+
+                {/* Toggler */}
+                <button
+                className='navbar-toggler'
+                type='button'
+                data-bs-toggle='collapse'
+                data-bs-target='#navbarCollapse'>
+                    
+                </button>
             </nav>
         </div>
     </div>
