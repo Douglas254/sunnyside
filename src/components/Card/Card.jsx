860831bdd33a9c1b0bef3876d6125ccfd99bf853
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-05-09 15:37:27 +0100

    edited card

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,16 @@
+// import Emily from '../../assets/image-emily.jpg'
+
+function Card({ children, ...props }) {
+
+
+    return (
+    <div>
+        <div className="container-xxl position-relative" {...props}>
+        </div>
+    </div>
+  )
+}
+
+export default Card
+
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import Card from "../components/Card/Card"
 import Hero from "../components/Hero/Hero"
 
 function Home() {
   return (
     <div>
       <Hero />
+      <Card />
     </div>
   )
 }
