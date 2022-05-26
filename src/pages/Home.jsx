7f256a0638ffa91a17133e293f38f1c7c967d3dc
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-05-26 22:41:26 +0100

    footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,10 @@
-<<<<<<< HEAD
-import Card from "../components/Card/Card"
-import Hero from "../components/Hero/Hero"
-
-=======
 import Hero from "../components/Hero/Hero";
-import Products from "../components/Products/Products";
->>>>>>> origin/main
+
 function Home() {
   return (
     <div>
       <Hero />
-<<<<<<< HEAD
-      <Card />
-=======
-      <Products />
->>>>>>> origin/main
+      
     </div>
   );
 }
